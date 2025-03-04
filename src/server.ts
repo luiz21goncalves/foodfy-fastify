5d769e7a2033d8d0bdbcc6fabc99fd9e8ae33e3f
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-03-04 14:52:06 -0300

    feat: add graceful shutdown

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,26 @@
 import { app } from "./app";
 
 app.listen({port: 3333, host: '0.0.0.0'}).then(address => {
-  console.log('Server running at:', address)
+  console.info('Server running at:', address)
 }).catch(err => {
   console.error(err)
+})
+
+const gracefulShutdown = () => {
+  app.close(() => process.exit())
+}
+
+process.on('SIGINT', gracefulShutdown)
+process.on('SIGTERM', gracefulShutdown)
+
+process.on('uncaughtException', (error, origin) => {
+  console.error(`uncaughtException origin ${origin}, ${error}`)
+})
+
+process.on('unhandledRejection', (error) => {
+  console.error(`unhandledRejection, ${error}`)
+})
+
+process.on('exit', (code) => {
+  console.info(`exit signal received with code ${code}`)
 })
