4578b2a17e570a442728f4b3e595597b234afa49
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-03-09 10:30:49 -0300

    feat: enble logs by enviroment variables

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,9 +1,10 @@
 import fastify from 'fastify'
 
+import { ENV } from '../env'
 import { routes } from './routes'
 
 const app = fastify({
-  logger: true,
+  logger: ENV.ENABLE_API_LOGGER,
 })
 
 app.register(routes, { prefix: 'v1' })
